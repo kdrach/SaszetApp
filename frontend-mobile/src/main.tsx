98b1a5ed31b05b3a3384f68c26f84dffece4bc1c
Krystian Drach <krystian.drach@gmail.com>
2026-07-08 10:24:31 +0200

    feat: integrate Keycloak auth on PWA root

--- frontend-mobile/src/main.tsx
+++ frontend-mobile/src/main.tsx
@@ -4,8 +4,18 @@ import App from './App.tsx';
 import './index.css';
 import './i18n';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import keycloak from './api/keycloak';
+
+keycloak.init({ onLoad: 'login-required', checkLoginIframe: false }).then((authenticated) => {
+  if (authenticated) {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  } else {
+    window.location.reload();
+  }
+}).catch((error) => {
+  console.error("Keycloak initialization failed", error);
+});
